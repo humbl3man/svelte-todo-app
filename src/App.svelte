bf7d4e8931d66efb651f0b06db681c57a2a27f2d
Konstantin Minevich <humbl3dev@gmail.com>
2023-01-09 20:10:24 -0800

    modal demo

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,16 @@
   import TodoForm from './lib/TodoForm.svelte';
   import TodoList from './lib/TodoList.svelte';
   import TodoStats from './lib/TodoStats.svelte';
+  import Modal from './lib/ui/Modal.svelte';
 
-  afterUpdate(() => {
-    console.log('App component updated');
-  });
+  let showAboutModal = false;
+
+  function toggleAboutModal() {
+    showAboutModal = !showAboutModal;
+  }
+  function handleAboutModalClose() {
+    showAboutModal = false;
+  }
 </script>
 
 <main class="h-screen w-full pt-16 bg-slate-500">
@@ -14,5 +20,26 @@
     <TodoForm />
     <TodoStats />
     <TodoList />
+    <div class="text-center mt-6">
+      <button class="underline text-blue-500 text-center" on:click={toggleAboutModal}>About this app</button>
+    </div>
   </div>
+
+  <Modal show={showAboutModal} onClose={handleAboutModalClose}>
+    <svelte:fragment slot="heading">About this App</svelte:fragment>
+    <svelte:fragment slot="text">
+      <p class="mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas vero iusto a eaque quae ullam perspiciatis earum tenetur eos? Voluptatum id suscipit veritatis nam
+        doloribus nisi unde, quasi nobis quis, maiores, harum nemo! Eveniet, enim vero. Fugiat expedita, dicta odio dolorem deleniti ratione beatae nesciunt necessitatibus magni,
+        quia rerum eveniet autem nobis ipsum deserunt rem voluptate earum totam laborum laudantium? Aliquid esse blanditiis, dolor quidem maxime, quasi, repudiandae perspiciatis
+        non ducimus maiores officia quisquam illo tempora. Ea reiciendis qui praesentium vel alias enim magni. In repellendus fuga quia mollitia perspiciatis deserunt illum, nam
+        minus possimus ullam sint iste enim voluptatibus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore cumque nesciunt tempora, rem dolor et nisi debitis expedita ipsa quos natus beatae voluptatibus harum
+        eum exercitationem sapiente atque eligendi aliquam! Perferendis accusamus laboriosam itaque tempora aspernatur quia, consequuntur fugit sint ad sequi fuga, officiis
+        mollitia ullam. Cupiditate earum officiis exercitationem?
+      </p>
+    </svelte:fragment>
+  </Modal>
 </main>
---
+++ src/lib/ui/Modal.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { blur, fade } from 'svelte/transition';
+
+  export let show = false;
+  export let onClose;
+</script>
+
+{#if show}
+  <div in:fade out:fade class="bg-black bg-opacity-75 fixed top-0 left-0 right-0 bottom-0 w-full h-full z-10">
+    <div in:blur class="bg-white p-16 max-w-lg mt-12 mx-auto rounded-md shadow-md relative">
+      <button class="absolute -top-4 right-0 bg-purple-500 text-white py-2 px-6 text-center rounded-md leading-none drop-shadow-lg sm:-right-4" on:click={() => onClose()}
+        >Close</button
+      >
+
+      <h1 class="font-bold text-2xl mb-5">
+        <slot name="heading" />
+      </h1>
+      <div class="leading-tight text-gray-700">
+        <slot name="text" />
+      </div>
+    </div>
+  </div>
+{/if}
